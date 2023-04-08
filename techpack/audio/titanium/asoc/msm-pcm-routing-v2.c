7cc7d4026d1c70dc20c0ba2c624ea14419380b3b
Giovanni Ricca <giovanniricca@proton.me>
2023-04-08 23:00:56 +0200

    titanium: ASoC: Import tas2557 codec
    From branch: daisy-p-oss
    
    Change-Id: I745be69acd387d1ec020c3978da68bf58c3ea534

--- techpack/audio/titanium/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/titanium/asoc/msm-pcm-routing-v2.c
@@ -19601,7 +19601,7 @@ static const struct snd_soc_dapm_route intercon[] = {
 
 	{"QUAT_MI2S_RX_DL_HL", "Switch", "QUAT_MI2S_DL_HL"},
 	{"QUAT_MI2S_RX", NULL, "QUAT_MI2S_RX_DL_HL"},
-#ifdef CONFIG_SND_SOC_MAX98927
+#if defined(CONFIG_SND_SOC_MAX98927) || defined(CONFIG_SND_SOC_TAS2557)
 	{"QUIN_MI2S_RX_DL_HL", "Switch", "PRI_MI2S_DL_HL"},
 #else
 	{"QUIN_MI2S_RX_DL_HL", "Switch", "QUIN_MI2S_DL_HL"},
@@ -19617,7 +19617,7 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"PRI_MI2S_RX", NULL, "PRI_MI2S_DL_HL"},
 	{"TERT_MI2S_RX", NULL, "TERT_MI2S_DL_HL"},
 	{"QUAT_MI2S_UL_HL", NULL, "QUAT_MI2S_TX"},
-#ifdef CONFIG_SND_SOC_MAX98927
+#if defined(CONFIG_SND_SOC_MAX98927) || defined(CONFIG_SND_SOC_TAS2557)
 	{"QUIN_MI2S_UL_HL", NULL, "QUIN_MI2S_TX"},
 #endif
 
