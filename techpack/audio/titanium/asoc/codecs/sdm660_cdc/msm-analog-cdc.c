6dc9dfde39658df04c54a8fd6a99f33eddfc90d2
Giovanni Ricca <giovanniricca@proton.me>
2023-04-09 21:01:04 +0200

    titanium: ASoC: Import remaining Xiaomi changes
    From branches: daisy-q-oss tissot-p-oss ysl-p-oss sakura-p-oss mido-n-oss
    
    Change-Id: I9f20622c7f52a39f67e8ccbed8a0038978ccb44e

--- techpack/audio/titanium/asoc/codecs/sdm660_cdc/msm-analog-cdc.c
+++ techpack/audio/titanium/asoc/codecs/sdm660_cdc/msm-analog-cdc.c
@@ -63,7 +63,13 @@
 #define SPK_PMD 2
 #define SPK_PMU 3
 
+#if (defined CONFIG_MACH_XIAOMI_TISSOT) || (defined CONFIG_MACH_XIAOMI_SAKURA) || (defined CONFIG_MACH_XIAOMI_DAISY) || (defined CONFIG_MACH_XIAOMI_VINCE)
+#define MICBIAS_DEFAULT_VAL 2200000
+#elif defined CONFIG_MACH_XIAOMI_YSL
+#define MICBIAS_DEFAULT_VAL 2750000
+#else
 #define MICBIAS_DEFAULT_VAL 1800000
+#endif
 #define MICBIAS_MIN_VAL 1600000
 #define MICBIAS_STEP_SIZE 50000
 
@@ -2887,6 +2893,9 @@ static int msm_anlg_cdc_lo_dac_event(struct snd_soc_dapm_widget *w,
 			MSM89XX_PMIC_ANALOG_RX_LO_DAC_CTL, 0x08, 0x08);
 		snd_soc_update_bits(codec,
 			MSM89XX_PMIC_ANALOG_RX_LO_DAC_CTL, 0x40, 0x40);
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
+		msleep(5);
+#endif
 		break;
 	case SND_SOC_DAPM_POST_PMU:
 		snd_soc_update_bits(codec,
